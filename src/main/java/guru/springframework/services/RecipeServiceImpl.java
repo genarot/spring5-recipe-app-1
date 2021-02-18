e9d104b9829e4046929efb996d31cbf6be0f744f
Tinoco, Genaro <genaro_13@hotmail.es>
2021-02-18 11:16:11 -0600

    Showing error message on error 404 page

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -40,8 +40,7 @@ public class RecipeServiceImpl implements RecipeService {
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
 
         if (!recipeOptional.isPresent()) {
-//            throw new RuntimeException("Recipe not found!");
-            throw new NotFoundException("Recipe not found!");
+            throw new NotFoundException("Recipe not found. For Id value: "+ id);
         }
         return recipeOptional.get();
     }
