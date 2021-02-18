e277f94d42c1cc94545ec763ace76921fc0295cc
Tinoco, Genaro <genaro_13@hotmail.es>
2021-02-18 11:24:29 -0600

    Adding handler for number format Exception

--- src/main/java/guru/springframework/controllers/RecipesController.java
+++ src/main/java/guru/springframework/controllers/RecipesController.java
@@ -75,4 +75,17 @@ public class RecipesController {
 
         return modelAndView;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormatException(Exception exception) {
+        log.error("Handling Number format exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", exception);
+        modelAndView.setViewName("400error");
+
+        return modelAndView;
+    }
 }
