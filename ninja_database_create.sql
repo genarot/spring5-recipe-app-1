3fe810ffdd2b2f58bcdd74aff9b9971761a47ba8
Tinoco, Genaro <genaro_13@hotmail.es>
2021-02-19 15:54:05 -0600

    Handling different database types and data files

---
+++ ninja_database_create.sql
@@ -0,0 +1,11 @@
+create table category (id bigint not null auto_increment, description varchar(255), primary key (id)) engine=MyISAM;
+create table ingredient (id bigint not null auto_increment, amount decimal(19,2), description varchar(255), recipe_id bigint, uom_id bigint, primary key (id)) engine=MyISAM;
+create table note (id bigint not null auto_increment, recipe_notes longtext, recipe_id bigint, primary key (id)) engine=MyISAM;
+create table recipe (id bigint not null auto_increment, cook_time integer, description varchar(255), difficulty varchar(255), directions longtext, image longblob, prep_time integer, servings integer, source varchar(255), url varchar(255), primary key (id)) engine=MyISAM;
+create table recipe_category (recipe_id bigint not null, category_id bigint not null, primary key (recipe_id, category_id)) engine=MyISAM;
+create table unit_of_measure (id bigint not null auto_increment, description varchar(255), primary key (id)) engine=MyISAM;
+alter table ingredient add constraint FKj0s4ywmqqqw4h5iommigh5yja foreign key (recipe_id) references recipe (id);
+alter table ingredient add constraint FK6iv5l89qmitedn5m2a71kta2t foreign key (uom_id) references unit_of_measure (id);
+alter table note add constraint FKaeln1ftj8676mm6e78iu7ibwq foreign key (recipe_id) references recipe (id);
+alter table recipe_category add constraint FKqsi87i8d4qqdehlv2eiwvpwb foreign key (category_id) references category (id);
+alter table recipe_category add constraint FKcqlqnvfyarhieewfeayk3v25v foreign key (recipe_id) references recipe (id);
--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -9,7 +9,7 @@ CREATE DATABASE sfg_prod;
 # Create database service accounts
 CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'ninja';
 CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'ninja';
-CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'ninja';
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'ninja';
 CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'ninja';
 
 
@@ -18,6 +18,9 @@ GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
 GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
 GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
 GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT CREATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DROP ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT ALTER ON sfg_dev.* to 'sfg_dev_user'@'localhost';
 GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
 GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
 GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
@@ -28,6 +31,10 @@ GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
 GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
 GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
 GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT CREATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT ALTER ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DROP ON sfg_dev.* to 'sfg_dev_user'@'%';
+
 GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
 GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
 GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
